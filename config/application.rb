202baf529d5775e5b9d5a22258dc474202de754d
cjjland76 <cjjland76@yahoo.co.jp>
2020-07-31 15:26:13 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ChatApp
       g.stylesheets false
       g.javascripts false
       g.helper false
-      f.test_framework false
+      g.test_framework false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
